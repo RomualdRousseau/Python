cc8f80791f2a5d7a0d5cc902b6a86eea52480094
Romuald Rousseau <romualdrousseau@gmail.com>
2022-08-19 22:25:18 +0800

    Fix and add pubsub notification

--- CloudFunctions/src/main/main.py
+++ CloudFunctions/src/main/main.py
@@ -1,10 +1,15 @@
 """ Test on Cloud Function and GCS
 """
 
+import os
 import time
 import requests
-import cloudstorage as gcs
-#import pandas as pd
+
+import pandas as pd
+
+from google.cloud import pubsub_v1
+from google.cloud import storage
+
 
 DEEPSEA_BASE_URL = "https://deepsealoader.servier.com.cn"
 
@@ -44,17 +49,18 @@ def dsl_get_collection(base_url, headers_with_auth, collection_name):
 
     # filter collection by name
 
-    by_name = lambda n, c: n == c["fileName"]
-    result = list(filter(lambda c: by_name(collection_name, c), collections))
+    result = list(
+            filter(
+                lambda c: collection_name == c["fileName"],
+                collections
+                )
+            )
 
-    if not result:
-        return None
-    else:
-        return result[0]
+    return result[0] if result else None
 
 
-def dsl_send_file(base_url, headers_with_auth, collection_id, file):
-    """ Send a file for tagging
+def dsl_upload_file(base_url, headers_with_auth, collection_id, file):
+    """ Upload a file for tagging
     """
 
     # Send a file to be tagged
@@ -72,6 +78,30 @@ def dsl_send_file(base_url, headers_with_auth, collection_id, file):
         files={"file": file}).text
 
 
+def dsl_download_file(base_url, headers_with_auth, file_id):
+    """ Download a file
+    """
+
+    # Check if result is ready
+
+    ready = False
+    while not ready:
+        time.sleep(1)
+        status = requests.get(
+            f"{base_url}/ftp/files/{file_id}",
+            headers=headers_with_auth
+            ).json()
+        ready = status["ready"]
+
+    # Download the result
+
+    return requests.get(
+        f"{base_url}/ftp/files/{file_id}/asfile",
+        headers=headers_with_auth,
+        stream=True
+        )
+
+
 def validate_message(message, param):
     """ validate_message
     """
@@ -85,41 +115,63 @@ def validate_message(message, param):
     return var
 
 
+def publish_message(publisher, topic, message):
+    """ Publish a message to a topic
+    """
+
+    project_id = os.environ["GCP_PROJECT"]
+    topic_path = publisher.topic_path(project_id, topic)
+    message_data = message.encode("utf-8")
+
+    futures = []
+    future = publisher.publish(topic_path, data=message_data)
+    futures.append(future)
+    for future in futures:
+        future.result()
+
+
 def on_new_file_inbox(file, _context):
     """ Send the file to DSL ML and save the result in GCS
     """
 
-    bucket = validate_message(file, "bucket")
-    filename = validate_message(file, "name")
+    bucket_name = validate_message(file, "bucket")
+    file_name = validate_message(file, "name")
     metadata = validate_message(file, "metadata")
-    collection = validate_message(metadata, "collection")
-    batchdate = validate_message(metadata, "batchdate")
+    collection_name = validate_message(metadata, "collection")
+    batch_date = validate_message(metadata, "batchdate")
 
-    headers_with_auth = dsl_get_auth_header(DEEPSEA_BASE_URL, USER_NAME, USER_PASS)
+    publisher = pubsub_v1.PublisherClient()
+    storage_client = storage.Client()
 
-    collection_id = dsl_get_collection(DEEPSEA_BASE_URL, headers_with_auth, collection)["id"]
+    headers_with_auth = dsl_get_auth_header(
+            DEEPSEA_BASE_URL, USER_NAME, USER_PASS)
+    collection_id = dsl_get_collection(
+            DEEPSEA_BASE_URL, headers_with_auth, collection_name).get("id")
 
-    with gcs.open(f"gs://{bucket}/{filename}") as gcs_file:
-        file_id = dsl_send_file(DEEPSEA_BASE_URL, headers_with_auth, collection_id, gcs_file)
+    blob = storage_client.get_bucket(bucket_name).blob(file_name)
+    with storage.fileio.BlobReader(blob) as gcs_file:
+        file_id = dsl_upload_file(
+                DEEPSEA_BASE_URL, headers_with_auth, collection_id, gcs_file)
 
-    # Check if result is ready
-
-    ready = False
-    while not ready:
-        time.sleep(1)
-        status = requests.get(
-            f"{DEEPSEA_BASE_URL}/ftp/files/{file_id}",
-            headers=headers_with_auth
-            ).json()
-        ready = status["ready"]
-
-    # Download the result
-
-    result = requests.get(
-        f"{DEEPSEA_BASE_URL}/ftp/files/{file_id}/asfile",
-        headers=headers_with_auth,
-        stream=True
-        )
-    with gcs.open(f"gs://outbox-gcp4affi/{collection}/{collection}-{batchdate}.csv", "w") as gcs_file:
-        for chunk in result.iter_content(chunk_size=128): 
+    file_data = dsl_download_file(DEEPSEA_BASE_URL, headers_with_auth, file_id)
+    bucket = storage_client.get_bucket("outbox-gcp4affi")
+    blob = bucket.blob(f"{collection_name}/{collection_name}-{batch_date}.csv")
+    with storage.fileio.BlobWriter(blob) as gcs_file:
+        for chunk in file_data.iter_content(chunk_size=128):
             gcs_file.write(chunk)
+
+    data = pd.read_csv(
+            f"gs://outbox-gcp4affi/{collection_name}/{collection_name}-{batch_date}.csv")
+    data.rename(columns={
+        '$id': 'id',
+        '$rowNum': 'rowNum',
+        '$sheetName': 'sheetName',
+        'date ($date)': 'date',
+        'customerCode ($customerCode)': 'customerCode',
+        'customerName ($customerName)': 'customerName',
+        'productCode ($productCode)': 'productCode',
+        'productName ($productName)': 'productName',
+        'address ($address)': 'address',
+        'quantity ($quantity)': 'quantity'
+        }, inplace=True)
+    publish_message(publisher, "new-file-outbox", data.head(1).to_json(orient="records"))

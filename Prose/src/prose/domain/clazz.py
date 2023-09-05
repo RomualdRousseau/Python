4a0b05cf07d92abfceb7d23e1dab4a06296ca96b
Romuald Rousseau <romualdrousseau@gmail.com>
2023-09-05 10:04:11 +0800

    Taiwan clean

--- Prose/src/prose/domain/clazz.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Class:
-    name: str
-    start_point: (int)
-    end_point: (int)
-    comment: (str) | None = None
--- Prose/src/prose/domain/method.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Method:
-    name: str
-    start_point: (int)
-    end_point: (int)
-    comment: (str) | None = None
-    test: (str) | None = None
